0ab853f4ce6e17cbe31996fe9d4189f56799bd7d
rahulkumarv269 <rahulkumarv269@gmail.com>
2023-05-15 23:12:26 +0530

    razerpay clone

---
+++ assets/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily : ["Mulish"],
+    },
+    colors:{
+      deepBlue: "#02042a",
+      LightBlue: "#2b84ea",
+      LightBlue300: "#4b94ed",
+      LightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText:"#818597",
+      LightGray:"#e2e2e2",
+      grayblue:"344a6c",
+      deepBluehead: "#162f56",
+      gray2:"#525a76",
+
+    },
+  },
+  plugins: [],
+}
+
